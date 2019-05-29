4f6150dedf72c8b9505c1853b7f225aaa6822d40
Caleb <47568685+caleb-ogbole@users.noreply.github.com>
2019-05-29 04:18:43 +0100

    Add files via upload

---
+++ analysis.js
@@ -0,0 +1,400 @@
+const workersanalysis= [
+    {
+        
+        FirstName:"Vanessa",
+        LastName:"Rogers",
+        Gender:"Female",
+        Age:19,
+        Salary: 60193,
+    },
+    {
+        FirstName:"Charlotte",
+        LastName:"Hunt",
+        Gender:"Female",
+        Age:24,
+        Salary: 92383,
+    },
+    {
+        FirstName:"Marcus",
+        LastName:"Foster",
+        Gender:"Male",
+        Age: 21,
+        Salary:65760,
+    },
+    {
+        FirstName:"Myra",
+        LastName:"Brooks",
+        Gender:"Female",
+        Age: 21,
+        Salary: 174036,
+    },
+    {
+        FirstName:"Caroline",
+        LastName:"Armstrong",
+        Gender:"Female",
+        Age: 29,
+        Salary:149879,
+    },
+    {
+        FirstName:"Belinda",
+        LastName:"Carroll",
+        Gender:"Female",
+        Age: 19,
+        Salary:188935,
+    },
+    {
+        FirstName:"Olivia",
+        LastName: "Armstrong",
+        Gender:"Female",
+        Age: 26,
+        Salary: 34782,
+    },
+    {
+        FirstName:"April",
+        LastName:"Cooper",
+        Gender:"Female",
+        Age: 20,
+        Salary: 68263,
+    },
+    {
+        FirstName:"Edith",
+        LastName:"Harrison",
+        Gender:"Female",
+        Age: 23,
+        Salary: 54709,
+    },
+    {
+        FirstName:"Vivian",
+        LastName:"Henderson",
+        Gender:"Female",
+        Age: 20,
+        Salary: 155709,
+    },
+    {
+        FirstName:"Ashton",
+        LastName:"Martin",
+        Gender: "Male",
+        Age: 27,
+        Salary: 166647,
+    },
+    {
+        FirstName:"Gianna",
+        LastName:"Moore",
+        Gender: "Female",
+        Age: 23,
+        Salary: 191619,
+    },
+    {
+        FirstName:"Camila",
+        LastName:"Allen",
+        Gender:"Female",
+        Age: 23,
+        Salary: 85567,
+    },
+    {
+        FirstName:"Gianna",
+        LastName:"Morrison",
+        Gender:"Female",
+        Age: 28,
+        Salary: 191509,
+    },
+    {
+        FirstName:"Paul",
+        LastName:"Cooper",
+        Gender: "Male",
+        Age: 28,
+        Salary: 39471,
+    },
+    {
+        FirstName:"Tara",
+        LastName:"Mason",
+        Gender: "Female",
+        Age: 19,
+        Salary: 189780,
+    },
+    {
+        FirstName:"Aida",
+        LastName:"Tucker",
+        Gender:"Female",
+        Age: 21,
+        Salary: 143187,
+    },
+    {
+        FirstName:"Richard",
+        LastName:"Cameron",
+        Gender:"Male",
+        Age: 21,
+        Salary: 116644,
+    },
+    {
+        FirstName:"Amy",
+        LastName:"Stewart",
+        Gender:"Female",
+        Age: 29,
+        Salary: 126961,
+    },
+    {
+        FirstName:"Eleanor",
+        LastName:"Thompson",
+        Gender: "Female",
+        Age: 20,
+        Salary: 159003,
+    },
+    {
+        FirstName:"Alberta",
+        LastName:"Edwards",
+        Gender:"Female",
+        Age: 21,
+        Salary: 105648,
+    },
+    {
+        FirstName:"Gianna",
+        LastName:"Evans",
+        Gender:"Female",
+        Age: 27,
+        Salary: 183805,
+    },
+    {
+        FirstName:"Florrie",
+        LastName:"Kelley",
+        Gender:"Female",
+        Age: 24,
+        Salary: 180568,
+    },
+    {
+        FirstName:"Miranda",
+        LastName:"Harris",
+        Gender: "Female",
+        Age: 19,
+        Salary: 185753,
+    },
+    {
+        FirstName:"Henry",
+        LastName:"Davis",
+        Gender:"Male",
+        Age: 18,
+        Salary: 83804,
+    },
+    {
+        FirstName:"Kirsten",
+        LastName:"Barnes",
+        Gender:"Female",
+        Age: 26,
+        Salary: 161833,
+    },
+    {
+        FirstName:"Jack",
+        LastName:"Chapman",
+        Gender: "Male",
+        Age: 19,
+        Salary: 41007,
+    },
+    {
+        FirstName:"Myra",
+        LastName:"Kelly",
+        Gender:"Female",
+        Age: 27,
+        Salary: 118346,
+    },
+    {
+        FirstName:"Emma",
+        LastName:"Evans",
+        Gender:"Female",
+        Age: 23,
+        Salary: 126125,
+    },
+    {
+        FirstName:"Blake",
+        LastName:"Evans",
+        Gender: "Male",
+        Age: 24,
+        Salary:69702,
+    },
+    {
+        FirstName:"Aldus",
+        LastName:"Armstrong",
+        Gender:"Male",
+        Age: 26,
+        Salary: 132491,
+    },
+    {
+        FirstName:"Adelaide",
+        LastName:"Martin",
+        Gender:"Female",
+        Age: 20,
+        Salary: 175263,
+    },
+    {
+        FirstName:"Victoria",
+        LastName:"Elliott",
+        Gender:"Female",
+        Age: 21,
+        Salary: 111645,
+    },
+    {
+        FirstName:"Nicole",
+        LastName:"Harrison",
+        Gender:"Female",
+        Age: 22,
+        Salary: 54624,
+    },
+    {
+        FirstName:"Edwin",
+        LastName:"Hamilton",
+        Gender:"Male",
+        Age: 18,
+        Salary: 152586,
+    },
+    {
+        FirstName:"Jessica",
+        LastName:"Robinson",
+        Gender: "Female",
+        Age: 29,
+        Salary: 85446,
+    },
+    {
+        FirstName:"Sofia",
+        LastName:"Turner",
+        Gender:"Female",
+        Age: 20,
+        Salary:131564,
+    },
+    {
+        FirstName:"Cadie",
+        LastName:"Higgins",
+        Gender:"Female",
+        Age: 21,
+        Salary: 138663,
+    },
+    {
+        FirstName:"Jessica",
+        LastName:"Turner",
+        Gender:"Female",
+        Age: 24,
+        Salary: 91319,
+    },
+    {
+        FirstName:"Walter",
+        LastName:"Hamilton",
+        Gender:"Male",
+        Age:25,
+        Salary:117408,
+    },
+    {
+        FirstName:"Alexander",
+        LastName:"Farell",
+        Gender:"Male",
+        Age: 26,
+        Salary: 65878,
+    },
+    {
+        FirstName:"Justin",
+        LastName:"Howard",
+        Gender:"Male",
+        Age: 21,
+        Salary: 165575,
+    },
+    {
+        FirstName:"Chelsea",
+        LastName:"Holmes",
+        Gender:"Female",
+        Age: 21,
+        Salary: 32690,
+    },
+    {
+        FirstName:"Alberta",
+        LastName:"Wilson",
+        Gender:"Female",
+        Age: 22,
+        Salary:143459,
+    },
+    {
+        FirstName:"Miller",
+        LastName:"Murphy",
+        Gender:"Male",
+        Age: 25,
+        Salary: 163776,
+    },
+    {
+        FirstName:"Adrianna",
+        LastName:"Hawkins",
+        Gender:"Female",
+        Age: 29,
+        Salary: 64412,
+    },
+    {
+        FirstName:"Dominik",
+        LastName:"Reed",
+        Gender: "Male",
+        Age: 23,
+        Salary: 42643,
+    },
+    {
+        FirstName:"Gianna",
+        LastName:"Craig",
+        Gender:"Female",
+        Age: 18,
+        Salary: 141944,
+    },
+    {
+        FirstName:"Alissa",
+        LastName:"Cameron",
+        Gender:"Female",
+        Age: 25,
+        Salary: 101581,
+    },
+    {
+        FirstName:"Aldus",
+        LastName:"Adams",
+        Gender:"Male",
+        Age: 28,
+        Salary: 132894,
+    },
+]
+//firstname and lastname of all the workers using foreach()
+console.log('First and Last names of all workers'.toUpperCase());
+const analysis =workersanalysis.forEach((analysis)=>{
+    let Name = `${analysis.FirstName} ${analysis.LastName}`;
+    console.log(Name);
+})
+//total salaries of all workers
+console.log('total salaries of all workers'.toUpperCase());
+const totalSalary = (total, workersanalysis) => {
+    return total + workersanalysis.Salary;
+};
+const sumSalary = workersanalysis.reduce(totalSalary, 0);
+console.log(sumSalary);
+//average age of workers using reduce
+console.log("average age of workers".toUpperCase());
+const age =workersanalysis.reduce((acc, curr)=> acc + curr.age ,0);
+const averageAge = age / 2 ;
+console.log(averageAge);
+
+//workers above 20
+//console.log('workers above twenty'.toUppercase());
+function speak(a){
+    return(a.age > 20);
+}
+const above20 = workersanalysis.filter(speak);
+console.log(above20);
+
+//double the salaries of workers
+console.log('doubled salaries of all workers'.toUpperCase());
+workersanalysis.map(b => b.Salary * 2).forEach(b => console.log(`Your salary is ${b}`));
+//assignment two
+console.log(" ");
+console.log(" assignment two".toUpperCase());
+//array as a parameter
+console.log('array as a parameter'.toUpperCase());
+const arr=[1,2,3,4,5];
+const arrParam=(arr)=>{console.log(`first element: ${arr[0]} , last element :${arr[4]}`)}
+arrParam(arr);
+//object as a parameter
+console.log('object as a parameter'.toUpperCase());
+let name = workersanalysis.forEach(x => {
+    console.log(`Your name is ${x.FirstName} ${x.LastName}, you're ${x.Age} year old, you're a ${x.Gender}, you earn ${x.Salary} `);
+});
+//name and score function
+console.log("name and score function".toUpperCase());
+let event = (name, score) =>{ return `Hello, ${name}, your score is ${score}`};
+console.log(event('Caleb', 700));
